7a6d959bec837f7455bc5069db772266ff189994
toomanu <manuyegon@gmail.com>
2023-02-16 21:33:33 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char String[] = "\"Programming is like building a multilingual puzzle\n";
+char String[] = "\"Programming is like building a multilingual puzzle";
 puts(String);
 return (0);
 }
