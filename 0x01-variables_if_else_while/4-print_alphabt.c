b8ae8494dfc04ef752a82993b71796822c552ceb
toomanu <manuyegon@gmail.com>
2023-02-17 21:47:26 +0300

    numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	for (a = 'a'; a <= 'z'; a++)
+		{
+			putchar(a,b,c);
+		}
+	putchar('\n');
+	return (0);
+}
