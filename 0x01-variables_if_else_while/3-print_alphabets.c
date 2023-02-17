c4e7b39f10f8040790f72335ad0b2762d67ff137
toomanu <manuyegon@gmail.com>
2023-02-17 21:18:37 +0300

    lower UPPER

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
