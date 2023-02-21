2f3bb1c76f3dd1ee624967c4aa7714244f11d484
toomanu <manuyegon@gmail.com>
2023-02-22 01:50:12 +0300

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs(int) - returns absolute value of integer
+ * @int: input character for checking
+ * Return: absolute int
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	n = (n * -1);
+	return (n);
+}
