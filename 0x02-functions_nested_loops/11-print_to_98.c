a73a90a744805006e1c1d38149c4bad948714e92
toomanu <manuyegon@gmail.com>
2023-02-22 02:33:37 +0300

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - returns absolute value of integer
+ * @a: first value
+ * @b: second value
+ * Return: additonal of a and b and return c
+ */
+
+void print_to_98(int n)
+{
+	while (n < 99)
+	{
+	_putchar(n);
+	n++;
+	}
+	_putchar('\n');
+}
