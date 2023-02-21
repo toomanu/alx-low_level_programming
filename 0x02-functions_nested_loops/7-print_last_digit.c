924ef67dfddf4a7883684a7e0b03adfd3c2fa078
toomanu <manuyegon@gmail.com>
2023-02-22 01:54:35 +0300

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: input character for checking
+ * Return: absolute int
+ */
+
+int print_last_digit(int n);
+{
+
+	n = (n % 10);
+	return (n);
+}
