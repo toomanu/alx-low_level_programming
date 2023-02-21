91900893f886d3aba472eb307be9ce4764cce216
toomanu <manuyegon@gmail.com>
2023-02-22 00:41:58 +0300

    alpha 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,15 @@
 
 void print_alphabet_x10(void)
 {
-	int a, i;
+	int a;
+	int i;
 
 	while (i < 10)
 	{
-	for (a = 'a'; a <= 'z'; a++)
-	{
-		_putchar(a);
-	}
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
 	_putchar('\n');
 	i++;
 	}
