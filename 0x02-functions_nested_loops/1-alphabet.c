e5a8f70a86e701c0333d77ff8612a3ec29cb44b8
toomanu <manuyegon@gmail.com>
2023-02-22 00:11:21 +0300

    lower alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return;
+}
