4d9c54eb7bce751d7a756923c7b00e6c864b7cd3
toomanu <manuyegon@gmail.com>
2023-02-22 01:26:31 +0300

    number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if number is positive or negative
+ * @n: input character for checking
+ * Return: 1 if @c is alphabet
+ */
+
+int print_sign(int n);
+{
+
+	if (n > 0)
+	_putchar('+');
+	return (1);
+	else if (n = 0)
+	_putchar('0');
+	return (1);
+	else if (n < 0)
+	_putchar('-');
+	return (0);
+}
+
