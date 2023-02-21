6433e2c096db96ec34fb656823996d0c710e303b
toomanu <manuyegon@gmail.com>
2023-02-22 01:02:46 +0300

    lower c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lower case 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c);
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (1);
+}
+
