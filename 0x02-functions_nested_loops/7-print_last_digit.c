1f86089ef85037eb77bc05090002d3c2f8f3f183
toomanu <manuyegon@gmail.com>
2023-02-22 20:53:39 +0300

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,23 @@
 /**
  * print_last_digit - print last digit
  * @n: input character for checking
- * Return: absolute int
+ * Return: last digit
  */
 
 int print_last_digit(int n)
 {
+	if (n > 0)
+	{
 	n = (n % 10);
-	_putchar (n);
-	return (n);
-	return (n);
+	_putchar(n);
+	}
+	else if (n < 0)
+	{
+	n = n * -1;
+	n = (n % 10);
+	_putchar(n);
+	}
+	else
+	_putchar('0');
+	return (0);
 }
