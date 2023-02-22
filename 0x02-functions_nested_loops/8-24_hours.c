6037f9bcfd88d33d106d5f29e2b804b552140540
toomanu <manuyegon@gmail.com>
2023-02-22 03:21:15 +0300

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet in lower case 10 times
+ * jack_bauer - print alphabet in lower case 10 times
  *
  * Return: Always 0 (Success)
  */
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
-
 	_putchar('\n');
 }
