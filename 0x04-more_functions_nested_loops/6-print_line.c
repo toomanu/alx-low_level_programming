93e5aa5f4e66582ad2fd9e7a876b6823a1089346
toomanu <manuyegon@gmail.com>
2023-02-24 01:53:05 +0300

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_line - print straight line
+ *
+ * @n: number of _ to be printed
+ *Return: 0
+ */
+
+void print_line(int n)
+{
+	int m;
+
+	if (n > 0)
+		for (m = 0; m <= n; m++)
+		{
+			{
+				_putchar('_');
+			}
+		}
+	else
+	{
+		_putchar('\n');
+	}
+}
