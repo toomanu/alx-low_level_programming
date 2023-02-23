51ef7bd28f7c09da3a7a44271933901931c31ebf
toomanu <manuyegon@gmail.com>
2023-02-23 23:43:01 +0300

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_islower - checks lower case
+ *@c: input character for checking
+ *Return: 0 if @c is lower
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'A')
+		return (1);
+	else
+		return (0);
+}
