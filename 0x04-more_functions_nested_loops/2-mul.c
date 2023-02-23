e3f616bdd4481211f856e0c0b325405faf124735
toomanu <manuyegon@gmail.com>
2023-02-24 00:02:23 +0300

    multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 
 /**
  *mul - multiplies two digits
- *@a: first digit 
+ *@a: first digit
  *@b: second digit
  *Return: 1 if @c is digit and 0 is @c is other
  */
@@ -10,6 +10,7 @@
 int mul(int a, int b)
 {
 	int c;
+
 	c = a * b;
 	return (c);
 }
