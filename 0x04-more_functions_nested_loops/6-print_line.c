c1bfa42f7e24e49f2f673acdd94130d1ed55787e
toomanu <manuyegon@gmail.com>
2023-02-24 01:58:39 +0300

    lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,7 @@
 /**
  *print_line - print straight line
  *
- * @n: number of _ to be printed
+ *@n: number of _ to be printed
  *Return: 0
  */
 
@@ -12,12 +12,14 @@ void print_line(int n)
 	int m;
 
 	if (n > 0)
-		for (m = 0; m <= n; m++)
+	{
+		for (m = 0; m < n; m++)
 		{
-			{
-				_putchar('_');
-			}
+			_putchar('_');
 		}
+
+		_putchar('\n');
+	}
 	else
 	{
 		_putchar('\n');
