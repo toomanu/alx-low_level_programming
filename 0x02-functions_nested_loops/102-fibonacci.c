ac17cea45e9da23ee0b3c546ae98c1542a0f445c
toomanu <manuyegon@gmail.com>
2023-02-25 01:59:06 +0300

    fib

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,23 +8,20 @@
 
 int main(void)
 {
+	int f1 = 0;
+	int f2 = 1;
+	int f3;
 	int i;
 	int n = 50;
 
-	int f1 = 1, f2 = 2;
-
-	int f3 = f1 + f2;
-
-
-	printf("%d, %d, ", f1, f2);
-
-	for (i = 3; i <= n; ++i)
+	printf("%d %d ", f1,f2); 
+	for (i = 2; i < n; i++)
 	{
-		printf("%d, ", f3);
+		f3 = f1 + f2;
+		printf("%d ", f3);
 		f1 = f2;
 		f2 = f3;
-		f3 = f1 + f2;
 	}
-
+	printf("\n");
 	return (0);
 }
