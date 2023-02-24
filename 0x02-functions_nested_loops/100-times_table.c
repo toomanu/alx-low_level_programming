d846a406d708a6a9c0d19e24793370ff610ea673
toomanu <manuyegon@gmail.com>
2023-02-25 01:18:35 +0300

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _isalpha - checks if letter is an alphabet
- * @c: input character for checking
- * Return: 1 if @c is alphabet
+ * print_times_table - checks if letter is an alphabet
+ * @n: input character for multiplying
+ * Return: 0
  */
 
 void print_times_table(int n)
 {
 	int m;
 	int p;
-	for ( n = '0'; n < '9'; n++)
+
+	for (n = '0'; n < '9'; n++)
 	{
-		for ( m = '0'; m < '9'; m++)
+		for (m = '0'; m < '9'; m++)
 		{
 			p = m * n;
 		_putchar(p);
