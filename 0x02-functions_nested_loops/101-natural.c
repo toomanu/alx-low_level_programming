a95ef8c8ff258d02f18d46454f1f81637fb0634a
toomanu <manuyegon@gmail.com>
2023-02-25 01:29:41 +0300

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print sum of multiples of 3 and 5
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 3; i < 1025; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+
+			sum = sum + i;
+	}
+
+	printf("%d\n", sum);
+
+}
+
