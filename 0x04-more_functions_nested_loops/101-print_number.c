987faecaf8627ccf27aae0919e7a51e9b6c5a0ca
toomanu <manuyegon@gmail.com>
2023-02-25 00:31:07 +0300

    int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,6 +11,8 @@
 
 void print_number(int n)
 {
+	if (n != 0)
+	{
 	if (n < 0)
 	{
 		_putchar('-');
@@ -23,4 +25,8 @@ void print_number(int n)
 	}
 
 	_putchar(n % 10 + '0');
+	}
+	else
+		_putchar('0');
+
 }
