01849d82c014fb51997d340c7675071d92789640
toomanu <manuyegon@gmail.com>
2023-02-27 21:56:13 +0300

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_puts - print string
+ *@str: input string for printing
+ *Return: 0
+ */
+
+void _puts(char *str)
+{
+
+	int i, j;
+
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	for (j = 0; j <= i; j++)
+	{
+		_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
+
