30b013d325962b5ba9c0913335d4b2581080865d
toomanu <manuyegon@gmail.com>
2023-02-27 21:51:45 +0300

    str rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_rev - print reverse of string
+ *@s: input string for reverse
+ *Return: 0
+ */
+
+void print_rev(char *s)
+{
+
+	int i, j;
+
+	i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
+
