2b163a882a10d3584fd7a71ee724e337199e04cb
toomanu <manuyegon@gmail.com>
2023-03-01 21:44:05 +0300

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strcat - concatenates two strings
+ *@dest: first string
+ *@src: inpsecond string
+ *Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+
+{
+	while (*dest)
+	{
+		dest++;
+	}
+
+	while (*src)
+	{
+		src++;
+		dest++;
+	}
+
+	*dest = '\0';
+
+	return (dest);
+}
