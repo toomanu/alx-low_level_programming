e76744a8a96af55b4a53bf6fde1ba8439827d0db
toomanu <manuyegon@gmail.com>
2023-03-08 22:19:07 +0300

    str length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_print_rev_recursion - Prints a string in reverse
+ *_strlen_recursion - Prints a string in reverse
  *@s: string to be counted
  *Return: 0
  */
@@ -9,9 +9,11 @@
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		return (0);
+	}
 	else
 	{
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 	}
 }
