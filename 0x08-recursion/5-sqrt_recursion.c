43a78608c321f39b9b01484195e07bd164e9c59c
toomanu <manuyegon@gmail.com>
2023-03-09 05:41:29 +0300

    prime

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_sqrt_recursion - returns value of x raised to the power of y
+ *@n: number to find square root
+ *Return: squareroot
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *is_prime_number - returns prime number
+ *@n: number to find prime number
+ *Return: prime number
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 0)
+	{
+		return (0);
+	}
+}
